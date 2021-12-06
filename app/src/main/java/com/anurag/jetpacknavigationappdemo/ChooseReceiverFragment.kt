d26960ec858988d43f06e24da131c6490d736997
anurag <kachhalaanurag@gmail.com>
2021-12-06 15:52:12 +0530

    add args

--- app/src/main/java/com/anurag/jetpacknavigationappdemo/ChooseReceiverFragment.kt
+++ app/src/main/java/com/anurag/jetpacknavigationappdemo/ChooseReceiverFragment.kt
@@ -1,8 +1,29 @@
 package com.anurag.jetpacknavigationappdemo
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_choose_receiver.*
 
 
 class ChooseReceiverFragment : Fragment(R.layout.fragment_choose_receiver) {
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController = findNavController()
+        val directions = ChooseReceiverFragmentDirections
+
+        sendBtn.setOnClickListener {
+            val receiverName = etReceiverName.text.toString().trim()
+
+           /* val args = Bundle()
+            args.putString("name",receiverName)*/
+            navController.navigate(directions.actionChooseReceiverFragmentToSendCashFragment(50,receiverName))
+
+        }
+
+
+    }
 }
--- app/src/main/java/com/anurag/jetpacknavigationappdemo/HomeFragment.kt
+++ app/src/main/java/com/anurag/jetpacknavigationappdemo/HomeFragment.kt
@@ -1,11 +1,40 @@
 package com.anurag.jetpacknavigationappdemo
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navDirections = HomeFragmentDirections
+        val navOption = NavOptions.Builder()
+            .setEnterAnim(R.anim.slide_in_right)
+            .setExitAnim(R.anim.slide_in_left)
+            .setPopEnterAnim(R.anim.slide_out_left)
+            .setExitAnim(R.anim.slide_out_right)
+            .build()
 
+        val navController = findNavController()
+        btnSendMoney.setOnClickListener {
+            //navController.navigate(R.id.action_homeFragment_to_viewBalanceFragment)
+            navController.navigate(navDirections.actionHomeFragmentToChooseReceiverFragment())
+        }
 
+        btnViewBalance.setOnClickListener {
+            navController.navigate(navDirections.actionHomeFragmentToViewBalanceFragment(),navOption)
+        }
+
+        btnViewTransation.setOnClickListener {
+            navController.navigate(navDirections.actionHomeFragmentToViewTransationFragment(),navOption)
+        }
+
+    }
 }
