7189122af8a772a7cfa559b32a5290829144556c
anurag <kachhalaanurag@gmail.com>
2021-12-06 19:23:16 +0530

    set up option menu

---
+++ app/src/main/java/com/anurag/jetpacknavigationappdemo/SettingFragment.kt
@@ -0,0 +1,21 @@
+package com.anurag.jetpacknavigationappdemo
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_setting.*
+
+
+class SettingFragment : Fragment(R.layout.fragment_setting) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+         amountEt.setText(SampleData.defaultAmount.value.toString())
+
+        btnSave.setOnClickListener {
+            val defaultAmount = amountEt.text.toString().toLong()
+            SampleData.defaultAmount.value = defaultAmount
+        }
+    }
+}
