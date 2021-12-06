ba2319c4084b210749addfba0b92af5a426ec71f
anurag <kachhalaanurag@gmail.com>
2021-12-06 16:24:49 +0530

    add dialog to navigation graph

--- app/src/main/java/com/anurag/jetpacknavigationappdemo/SendCashFragment.kt
+++ app/src/main/java/com/anurag/jetpacknavigationappdemo/SendCashFragment.kt
@@ -3,6 +3,7 @@ package com.anurag.jetpacknavigationappdemo
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import kotlinx.android.synthetic.main.fragment_send_cash.*
 
@@ -14,7 +15,16 @@ class SendCashFragment : Fragment(R.layout.fragment_send_cash) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         // val receiverName = arguments?.getString("name")
+        val navController = findNavController()
+        val directAction = SendCashFragmentDirections
         val receiverName = args.receiverName
         tvReceiverName.text = "Send cash to $receiverName"
+
+        val amount = etMoney.text
+
+        send.setOnClickListener {
+           navController.navigate(directAction.actionSendCashFragmentToConfimDialogFragment(receiverName, amount = amount.toString().toLong()))
+        }
+
     }
 }
