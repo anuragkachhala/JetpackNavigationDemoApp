42f5e2e038c543fd069e22215a88f5d92b86e10a
anurag <kachhalaanurag@gmail.com>
2021-12-06 17:38:15 +0530

    setup navigation up

--- app/src/main/java/com/anurag/jetpacknavigationappdemo/ConfimDialogFragment.kt
+++ app/src/main/java/com/anurag/jetpacknavigationappdemo/ConfimDialogFragment.kt
@@ -4,11 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import kotlinx.android.synthetic.main.fragment_choose_receiver.*
 import kotlinx.android.synthetic.main.fragment_dialog_confirm.*
 
 
--- app/src/main/java/com/anurag/jetpacknavigationappdemo/MainActivity.kt
+++ app/src/main/java/com/anurag/jetpacknavigationappdemo/MainActivity.kt
@@ -2,10 +2,29 @@ package com.anurag.jetpacknavigationappdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+
+        // nav controller is connected to nav host so will get nav controller from nav host
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
